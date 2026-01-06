f05dc4ab6465c8dc8c970e6141b80e5b5c380450
slapthedodo <102239541+slapthedodo@users.noreply.github.com>
2026-01-06 17:57:07 +0100

    Update bsswadimsticker.lua

--- bsswadimsticker.lua
+++ bsswadimsticker.lua
@@ -657,7 +657,7 @@ task.spawn(function()
                 local upRotation = CFrame.Angles(math.rad(90), 0, 0)
                 
                 
-                local targetY = 285
+                local targetY = 280
                 
                 local TargetSlimeBlob = nil
                 for i = 1, 14 do
